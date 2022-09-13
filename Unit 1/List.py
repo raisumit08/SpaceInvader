fda8a921728ae9160c0d049d807ce6333a6f7090
raisumit08 <raisumitur@gmail.com>
2022-09-13 22:59:04 +0530

    12-09

---
+++ Unit 1/List.py
@@ -0,0 +1,6 @@
+L=[]
+n=int(input("Enter total number of elements you want to enter in list: "))
+for i in range(n):
+    a=int(input())
+    L.append(a)
+print(L)
---
+++ Unit 1/Untitled-4.py
@@ -0,0 +1,3 @@
+p=int(input("Enter a number"))
+for i in range (p):
+    print("hi \n")
