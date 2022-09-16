13d77093472a0a2b40d592f51a5c65b60cea5ac0
raisumit08 <raisumitur@gmail.com>
2022-09-16 22:09:31 +0530

    Operators in Python

--- Unit 1/M1_OtherFunctions.py
+++ Unit 1/M1_OtherFunctions.py
@@ -1,18 +1,33 @@
 def add(x,y):
     return x+y
+    '''
+    This function add two variables a & b 
+    and returns the sum of a & b
+    '''
 
 def sub(x,y):
     return (x-y)
+    '''
+    This function subtracts two variables a & b 
+    and returns the subtraction of a & b
+    '''
 
 print("Hello world 1")
 
 def main():
-    print("Hello world main")
+    '''
+    This function is testig all the functionalities
+    '''
+    if add(1,2) == 3:
+        print("add fun is working fine")
+    if sub(3,2) == 1:
+        print("sub fun is working fine")
 
-main()
+print(__name__)
 
 if __name__ =="__main__":
-    print("Hello wordld 2")
     main()
 
 print("Hello world 3")
+
+#regular expression, API document
---
+++ Unit 1/M1_Task-05_ArithmeticOperatorAndExpressionInPython.py
@@ -0,0 +1,40 @@
+#Arithmetic Operator
+'''
+Addition[+]
+Subtraction[-]
+Multiplicaton[*]
+Division[/]
+Modulus[%]
+Exponentiation[**]
+Floor Division[//]
+'''
+
+val1=2
+val2=3
+
+res = val1 + val2
+print(res)
+
+res = val1 - val2
+print(res)
+
+res = val1 * val2
+print(res)
+
+res = val1 / val2
+print(res)
+
+res = val1 % val2
+print(res)
+
+res = val1 ** val2
+print(res)
+
+res = val1 // val2
+print(res)
+
+
+
+#python -m pydoc -w(webpage) Sample.py
+
+#Sample.html
